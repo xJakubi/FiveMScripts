636115996f55706ae9d271e48a8df0acfe6a7a44
xJakubi <142356569+xJakubi@users.noreply.github.com>
2025-01-30 21:45:40 +0000

    Add files via upload

---
+++ loadingscreen/esx_loadingscreen/fxmanifest.lua
@@ -0,0 +1,17 @@
+game 'common'
+
+fx_version 'cerulean'
+author 'ESX-Framework'
+description 'Allows resources to Run tasks at specific intervals.'
+version '1.12.4'
+lua54 'yes'
+
+loadscreen 'index.html'
+
+shared_script 'config.lua'
+
+loadscreen_manual_shutdown "yes"
+
+client_script 'client/client.lua'
+
+files { 'index.html', './vid/*.mp4', './vid/*.webm', './js/index.js', './css/index.css' }
